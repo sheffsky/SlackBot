892720de741064c6e2d049e5d8313ec24d9f95fe
Sheffsky <elfcorp@gmail.com>
2016-01-27 22:03:34 +0500

    refactoring

--- SlackBotDNS320.py
+++ SlackBotDNS320.py
@@ -12,28 +12,25 @@ dir_list_command = Config.get('EnvironmentOptions', 'dir_list_command')
 
 
 def main():
-    timestamp = ''
+    timestamp = time.time()
     slack_bot_dns320 = SlackBot(slack_token, channel_id, file_with_list, dir_list_command)
     try:
-        #  getting the latest message timestamp
-        msgs = slack_bot_dns320.get_sorted_latest_messages(timestamp)
-        if msgs:  # not empty
-            timestamp = msgs[-1]['ts']  # here it is
         slack_bot_dns320.post_message('Ready. Type *help* to get started')
+
+        while True:
+            time.sleep(5)
+            try:
+                msgs = slack_bot_dns320.get_sorted_latest_messages(timestamp)
+                if msgs:  # not empty
+                    timestamp = msgs[-1]['ts']
+                    for msg in msgs:
+                        if 'bot_message' not in msg:
+                            slack_bot_dns320.parse_command(msg['text'])
+            except Exception as e:
+                print e
+                slack_bot_dns320.post_message(str(e))
+
     except Exception as e:
         print e
-        slack_bot_dns320.post_message(str(e))
-
-    while True:
-        time.sleep(5)
-        try:
-            msgs = slack_bot_dns320.get_sorted_latest_messages(timestamp)
-            if msgs:  # not empty
-                timestamp = msgs[-1]['ts']
-                for msg in msgs:
-                    if 'bot_message' not in msg:
-                        slack_bot_dns320.parse_command(msg['text'])
-        except Exception as e:
-            print e
 
 main()
